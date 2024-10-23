efa04cb478c7352d012866d31abb8c3b46e20cd5
MonicaAlvesP <monica52782@gmail.com>
2024-10-23 09:10:03 -0300

    Aplicando .gitignore

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Olá, Pessoal!");
+
+app.MapPost("/login", (minimal_api.Dominio.DTOs.LoginDTO loginDTO) =>
+{
+  if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+  {
+    return Results.Ok("Login realizado com sucesso!");
+  }
+  else
+  {
+    return Results.Unauthorized();
+  }
+});
+
 
 app.Run();
